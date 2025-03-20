3232a10d9fcde36f71599805e344c44ab38adad8
Kipngeno Abednego <abnasgit@gmail.com>
2025-03-20 16:58:54 +0300

    initial commit

---
+++ Python/if_statements/applications.py
@@ -0,0 +1,49 @@
+
+
+
+a = 10
+b = 5
+c = 10
+text = "hello"
+list_x = [1,2,3,4,5]
+list_y = [1,2,3,4,5]
+list_z = list_x
+
+# if a > b and c >= a:
+#     print("a is greater than b AND c is greater than or equal to a")
+# elif a <= b or c != b:
+#     print("Either a is less than OR equal to b and c is not equal to b")
+# else:
+#     print("None of the conditions were met")
+
+# if list_x  == list_y and list_x is not list_y        :
+#     print("list_x and list_y has the same values but are not the same object")
+# elif not(a==b):
+#     print("a is not equal to b")
+# else:
+#     print("None of the conditions were met")
+
+
+# if "e" in text and 6 not in list_x:
+#     print("letter e is in text and number 6 is not in list_x")
+# elif "x" not in text or 3 in list_x:
+#     print("Either letter 'x' is not in text OR number 3 is in the list_x")
+
+# else:
+#     print("none of the membership conditions were met")
+
+
+
+a = 10
+b = 5
+c = 10
+text = "hello"
+list_x = [1,2,3,4,5]
+list_y = [1,2,3,4,5]
+list_z = list_x
+
+
+# if a > b and (3 in list_x or text is not None):
+#       print("complex condition met: a is greater than b AND 3 is in list_x OR text is not None")
+if list_x is list_z and len(text) > 0:
+      print("list_x and list_z are the same object AND the length of text is greater than 0")
