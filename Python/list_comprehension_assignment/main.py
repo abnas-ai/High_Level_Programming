ee5b573ed80d914879afd2773714da7df9807edd
Kipngeno Abednego <abnasgit@gmail.com>
2025-03-25 23:21:38 +0300

    a guide on list comprehensions

---
+++ Python/list_comprehension_assignment/main.py
@@ -0,0 +1,130 @@
+
+# fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+# newlist = []
+
+# for x in fruits:
+#   if "a" in x:
+#     newlist.append(x)
+
+# print(newlist)
+
+
+# fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+# newlist = [x for x in fruits if "a" in x]
+
+# print(newlist)  
+
+
+
+
+
+
+
+
+
+
+# teachers = ["Getrude", "Tom", "Mufti", "Kangogo", "Obama"]
+# newteachers = []
+# for y in teachers:
+#     if "T" in y:
+#         newteachers.append(y)
+# print(newteachers)
+        
+
+
+# teachers = ["Getrude", "Tom", "Mufti", "Kangogo", "Obama"]
+
+# newteachers = [y for y in teachers if "t" in y]
+
+# print(newteachers)
+      
+         
+
+
+
+
+
+
+
+
+
+
+
+# dj = ["bash", "kalonje", "masumbuko", "lyta"]
+# newdj = []
+# for y in dj:
+#     if "k" in y:
+#         newdj.append(y)
+# print(newdj)
+
+
+
+# dj = ["bash", "kalonje", "masumbuko", "lyta"]
+# newdj = [y for y in dj if "k" in y]
+# print(newdj)
+
+# dj = ["masumbuko", "lyta", "kalonje", "bash"]
+# newdj = []
+# for y in dj:
+#     if "k" in y:
+#         newdj.append(y)
+# print(newdj)
+
+
+
+# dj = ["masumbuko", "kalonje", "lyta", "bash"]
+# newdj = []
+# for y in dj:
+#     if "k" in y:
+#         newdj.append(y)
+# print(newdj)
+
+
+# dj = ["masumbuko", "kalonje", "lyta", "bash"]        
+# newdj = [y for y in dj if "k" in y]
+# print(newdj)
+
+
+# USE CASE 4
+# a = [1, 2, 3, 4, 5]
+
+# # Create an empty list 'res' to store results
+# res = []
+
+# # Iterate over each element in list 'a'
+# for val in a:
+  
+#     # Multiply each element by 2 and append it to 'res'
+#     res.append(val * 2)
+
+# print(res)
+
+
+#Comprehended list
+# a = [1, 2, 3, 4, 5]
+
+# res = [val * 5 for val in a]
+
+# print(res)
+
+
+
+
+
+
+
+
+# a = [1, 2, 3, 4, 5]
+# change = []
+# for num in a:
+#     change.append(num*2)
+# print(change)
+
+
+a = [1, 2, 3, 4, 5]
+change = [y * 2 for value in a]
+print(change)
+
+
+
