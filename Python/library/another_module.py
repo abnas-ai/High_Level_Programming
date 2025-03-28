0a52cd2fedd02e13fba447d9237592b946791d04
Kipngeno Abednego <abnasgit@gmail.com>
2025-03-28 15:18:43 +0300

    iintroduction to modules packages and libraries

---
+++ Python/library/another_module.py
@@ -0,0 +1,9 @@
+
+from modules import calculator
+
+result = calculator("subtract")(10, 5)
+print(result)
+
+result2 = calculator("multiply")(10, 5)
+print(result2)
+
---
+++ Python/library/run.py
@@ -0,0 +1,9 @@
+
+# import math
+# PI = math.pi
+# print(PI)
+
+import time
+print("Hello world")
+time.sleep(5)
+print("THis is going to take 5 seconds....")
