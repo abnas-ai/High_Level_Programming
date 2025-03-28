c2ca65ee64b57400d0729d2d321d7b7e81b73b24
Kipngeno Abednego <abnasgit@gmail.com>
2025-03-28 15:16:14 +0300

    for loop practice methods

--- Python/for_loop_assignment/main.py
+++ Python/for_loop_assignment/main.py
@@ -2,8 +2,19 @@
 
 #Create a multiplication table in a clear and organised manner. use for loop
 
-number = 1
-for y in range(1, 10):
+# number = 1
+# for y in range(1, 10):
     
     
- print(f"{number} x {y} = {number*y}")
+#  print(f"{number} x {y} = {number*y}")
+ 
+ 
+# a = [1, 2, 3, 4, 5]
+# change = []
+# for y in a:
+#     change.append(y*2)
+# print(change)
+
+# a = [1, 2, 3, 4, 5]
+# change = [y*2 for y in a]
+# print(change)
