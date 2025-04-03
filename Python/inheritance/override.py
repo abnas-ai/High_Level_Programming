7280eabc1c609e5206a9448b8c0f1e393cc3bd9e
Kipngeno Abednego <abnasgit@gmail.com>
2025-04-03 09:09:10 +0300

    initial commit

--- Python/inheritance/override.py
+++ Python/inheritance/override.py
@@ -1,7 +1,7 @@
 
 from models import ElectricCar
 
-my_tesla = ElectricCar("Tesla", "MOdel S", 2022, 100)
+my_tesla = ElectricCar("Tesla", "Model S", 2022, 100)
 
 print(my_tesla.get_info())
 print(my_tesla.start_engine())
